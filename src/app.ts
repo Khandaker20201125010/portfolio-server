075c2ca81d344be6c209c70dd7d321ec9bbd1cad
unknown <prantokih42@gmail.com>
2025-09-28 20:23:14 +0600

    app and server added

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/blogs', blogRoutes);
+// app.use('/api/projects', projectRoutes);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
